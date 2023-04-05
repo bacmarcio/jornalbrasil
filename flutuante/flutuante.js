28ad952da8c6ad6b41d5e402b45d207dea2aace9
Márcio Maia <bac.marcio@gmail.com>
2023-04-05 00:03:44 -0300

    zerando tudo

--- flutuante/flutuante.js
+++
@@ -1,7 +0,0 @@
-function mostra(){
-        if(document.getElementById('mostraLigue').style.display == 'inline-block'){
-            document.getElementById('mostraLigue').style.display = 'none';
-        }else{
-            document.getElementById('mostraLigue').style.display = 'inline-block';
-        }
-    }
